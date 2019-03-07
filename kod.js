b687395f46a92aa118a442d9bfe6c4524f5e3e9e
opkorny <l.korzonek@seznam.cz>
2019-03-07 23:52:19 +0100

    stale nedokonceno

---
+++ kod.js
@@ -0,0 +1,62 @@
+$(function () {
+    // google
+    document.getElementById("googleButton").addEventListener("click", function () {
+        var google = "https://www.google.com/search?q=" + document.getElementById("google").value;
+        location.href = google;
+    });
+    document.getElementById("google").addEventListener("keydown", function (e) {
+        var google = "https://www.google.com/search?q=" + document.getElementById("google").value;
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            location.href = google;
+        }
+    });
+    // seznam
+    document.getElementById("seznamButton").addEventListener("click", function () {
+        var seznam = "https://search.seznam.cz/?q=" + document.getElementById("seznam").value;
+        location.href = seznam;
+    });
+    document.getElementById("seznam").addEventListener("keydown", function (e) {
+        var seznam = "https://search.seznam.cz/?q=" + document.getElementById("seznam").value;
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            location.href = seznam;
+        }
+    });
+    // cz.nic
+    document.getElementById("nicButton").addEventListener("click", function () {
+        var nic = "https://www.nic.cz/whois/domain/" + document.getElementById("nic").value;
+        location.href = nic;
+    });
+    document.getElementById("nic").addEventListener("keydown", function (e) {
+        var nic = "https://www.nic.cz/whois/domain/" + document.getElementById("nic").value;
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            location.href = nic;
+        }
+    });
+    // TinyURL
+    document.getElementById("urlButton").addEventListener("click", function () {
+        var url = "https://tinyurl.com/create.php?source=&url=" + document.getElementById("url").value;
+        location.href = url;
+    });
+    document.getElementById("url").addEventListener("keydown", function (e) {
+        var url = "https://tinyurl.com/create.php?source=&url=" + document.getElementById("url").value;
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            location.href = url;
+        }
+    });
+    // js
+    document.getElementById("jsButton").addEventListener("click", function () {
+        var js = document.getElementById("js").value;
+        document.getElementById('kod').innerHTML = js;
+    });
+    document.getElementById("js").addEventListener("keydown", function (e) {
+        var js = document.getElementById("js").value;
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            document.getElementById('kod').innerHTML = js;
+        }
+    });
+});
